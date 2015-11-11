84454240f56288947987b5fb6d845d60f5c2a29d
Ya Zhuang <zhuangya@gmail.com>
2015-11-11 15:43:47 +0800

    rewrite in a modern way
    - add license
    - travis test case
    - modern mocha
    - negativie side test case

--- test.js
+++ test.js
@@ -1,8 +1,12 @@
 var assert = require('assert');
-var isCredit = require('../index');
+var isCredit = require('.');
 
 describe('isCreditCard', function () {
+  it('should throw error when credit card is invalid', function () {
+    assert.throws(function () { isCredit('123') }, /invalid card number/);
+  });
   it('should return true when the credit card is valid', function () {
     assert.equal(isCredit('4367455035219281'), true);
   });
 });
+
