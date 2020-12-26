ad197e161fcfff7126701f687b8989654b7ef118
Ya Zhuang <zhuangya@users.noreply.github.com>
2020-12-27 01:25:55 +0800

    rewrite with math method

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
-'use strict';
+"use strict";
 
-module.exports = function isCreditCard (cardNumber) {
-  cardNumber = cardNumber.split('');
-  if (cardNumber.length !== 16) {
-    throw new Error('invalid card number');
-  }
-
-  return cardNumber.map(function (num, index) {
-    return (index % 2 === 0 ? num * 2 : num).toString();
-  }).reduce(function (soFar, strnum) {
-    strnum.split('').forEach(function (sn) {
-      soFar = soFar + parseInt(sn, 10);
-    });
-
-    return soFar;
-  }, 0) % 10 === 0;
+module.exports = function isCreditCard(cardNumber) {
+  return (
+    [...cardNumber].reduce((sum, digit, index) => {
+      digit = digit * (index % 2 === 0 ? 2 : 1);
+      while (digit >= 10) {
+        sum += digit % 10;
+        digit = Math.floor(digit / 10);
+      }
+      return sum + digit;
+    }, 0) %
+      10 ===
+    0
+  );
 };
